f6276f0bb7549eab305d8008d3c24eb6bbdda6f0
xairaven <alexkovalevkpi@gmail.com>
2024-07-21 19:22:54 +0300

    (Chapter 2) Behavioural Patterns: Visitor

---
+++ 02-Desing-Patterns/01-Behavioural-Patterns/06-Visitor/02-Void.rs
@@ -0,0 +1,13 @@
+pub fn walk_expr(visitor: &mut Visitor, e: &Expr) {
+    match *e {
+        Expr::IntLit(_) => {}
+        Expr::Add(ref lhs, ref rhs) => {
+            visitor.visit_expr(lhs);
+            visitor.visit_expr(rhs);
+        }
+        Expr::Sub(ref lhs, ref rhs) => {
+            visitor.visit_expr(lhs);
+            visitor.visit_expr(rhs);
+        }
+    }
+}
