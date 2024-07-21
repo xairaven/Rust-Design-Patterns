5bfa81884330df6b3fbab589b8a4ab9825296b07
xairaven <alexkovalevkpi@gmail.com>
2024-07-21 15:29:12 +0300

    (Chapter 2) Behavioural Patterns: Newtype

---
+++ 02-Desing-Patterns/01-Behavioural-Patterns/03-Newtype/01-Password-Type.rs
@@ -0,0 +1,17 @@
+use std::fmt::Display;
+
+// Create Newtype Password to override the Display trait for String
+struct Password(String);
+
+impl Display for Password {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "****************")
+    }
+}
+
+fn main() {
+    let unsecured_password: String = "ThisIsMyPassword".to_string();
+    let secured_password: Password = Password(unsecured_password.clone());
+    println!("unsecured_password: {unsecured_password}");
+    println!("secured_password: {secured_password}");
+}
